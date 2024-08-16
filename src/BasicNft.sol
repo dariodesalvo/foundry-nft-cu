889a2e188e16eea337e1901db35d64a34ad84cc7
dariodesalvo <dariodesalvo@gmail.com>
2024-08-16 14:47:13 -0300

    setup project

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,4 +2,23 @@
 
 pragma solidity ^0.8.18;
 
-contract BasicNft {}
+import {ERC721} from "@openzeppelin/contracts//token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "Dog") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft()  public {
+        
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory){
+        return "";
+    }
+}
