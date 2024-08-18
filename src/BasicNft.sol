eed2907ab461ad4b12ecfc0282e7288cf18752e2
dariodesalvo <dariodesalvo@gmail.com>
2024-08-17 21:16:27 -0300

    deploy script :rocket:, more tests :white_check_mark:

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -7,19 +7,22 @@ import {ERC721} from "@openzeppelin/contracts//token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
 
     uint256 private s_tokenCounter;
+    mapping (uint256 => string) s_tokenIdToUri;
 
     constructor() ERC721("Dogie", "Dog") {
         s_tokenCounter = 0;
     }
 
-    function mintNft()  public {
-        
+    function mintNft(string memory _tokenURI)  public {
+        s_tokenIdToUri[s_tokenCounter] = _tokenURI;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
     function tokenURI(
         uint256 _tokenId
     ) public view override returns (string memory){
-        return "";
+        return s_tokenIdToUri[_tokenId];
     }
 
     function getTokenCounter() public view returns(uint256) {
